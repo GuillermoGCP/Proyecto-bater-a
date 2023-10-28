e57c6bf3e6a2b8b325d774ecdc4cba8828ddacbf
Guillermo C Porto <guillermocporto@gmail.com>
2023-10-28 17:38:30 +0200

    Añadido el script para tocar batería con teclas del teclado y un bloque de try/catch en la función de reproducción

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 "use strict";
 
-//MÓDULO REPRODUCCIÓN DE SONIDOS INDIVIDUALES:
-
 //Selectors:
 const buttons = document.querySelectorAll("#buttonsContainer > button");
 const audios = document.querySelectorAll("#audiosContainer > audio");
 
-//Styles and play:
+//Styles and click play:
 for (let btn of buttons) {
   btn.addEventListener("click", () => {
     //STYLES:
@@ -27,7 +25,45 @@ function playSound(id) {
   for (let audio of audios) {
     const sound = audio;
     if (id === sound.className) {
-      sound.play();
+      try {
+        sound.play();
+      } catch (error) {
+        console.error(`Error al reproducir el audio: ${error.message}`);
+      }
     }
   }
 }
+
+//Keys play:
+document.body.addEventListener("keydown", (event) => {
+  for (let audio of audios) {
+    if (event.key === "Enter" && audio.className === "snare") {
+      audio.play();
+      break;
+    }
+    if (event.key === " " && audio.className === "kick") {
+      audio.play();
+    }
+    if (event.key === "t" && audio.className === "tom_high") {
+      audio.play();
+    }
+    if (event.key === "r" && audio.className === "tom_mid") {
+      audio.play();
+    }
+    if (event.key === "e" && audio.className === "tom_low") {
+      audio.play();
+    }
+    if (event.key === "p" && audio.className === "crash") {
+      audio.play();
+    }
+    if (event.key === "o" && audio.className === "hihat_close") {
+      audio.play();
+    }
+    if (event.key === "i" && audio.className === "hihat_open") {
+      audio.play();
+    }
+    if (event.key === "u" && audio.className === "ride") {
+      audio.play();
+    }
+  }
+});
