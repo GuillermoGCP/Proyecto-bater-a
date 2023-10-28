2783f8d09a903f8f48ca7606308a95d2aa7303d0
Guillermo C Porto <guillermocporto@gmail.com>
2023-10-28 16:29:03 +0200

    añado js simplificado y con estilos

--- js/main.js
+++ js/main.js
@@ -2,105 +2,32 @@
 
 //MÓDULO REPRODUCCIÓN DE SONIDOS INDIVIDUALES:
 
-//Buttons:
-const crashButton = document.getElementById("crashButton");
-const hithat_closeButton = document.getElementById("hihat_closeButton");
-const hithat_openButton = document.getElementById("hihat_openButton");
-const kickButton = document.getElementById("kickButton");
-const rideButton = document.getElementById("rideButton");
-const snareButton = document.getElementById("snareButton");
-const tom_highButton = document.getElementById("tom_highButton");
-const tom_lowButton = document.getElementById("tom_lowButton");
-const tom_midButton = document.getElementById("tom_midButton");
+//Selectors:
+const buttons = document.querySelectorAll("#buttonsContainer > button");
+const audios = document.querySelectorAll("#audiosContainer > audio");
 
-//Audios:
-const audioCrash = new Audio("../audio/crash.wav");
-const audioHihat_close = new Audio("../audio/hihat-close.wav");
-const audioHihat_open = new Audio("../audio/hihat-open.wav");
-const audioKick = new Audio("../audio/kick.wav");
-const audioRide = new Audio("../audio/ride.wav");
-const audioSnare = new Audio("../audio/snare.wav");
-const audioTom_high = new Audio("../audio/tom-high.wav");
-const audioTom_low = new Audio("../audio/tom-low.wav");
-const audioTom_mid = new Audio("../audio/tom-mid.wav");
-
-//Events with click:
-crashButton.addEventListener("click", () => {
-  audioCrash.play();
-  crashButton.blur();
-});
-hithat_closeButton.addEventListener("click", () => {
-  audioHihat_close.play();
-  hithat_closeButton.blur();
-});
-hithat_openButton.addEventListener("click", () => {
-  audioHihat_open.play();
-  hithat_openButton.blur();
-});
-kickButton.addEventListener("click", () => {
-  audioKick.play();
-  kickButton.blur();
-});
-rideButton.addEventListener("click", () => {
-  audioRide.play();
-  rideButton.blur();
-});
-snareButton.addEventListener("click", () => {
-  audioSnare.play();
-  snareButton.blur();
-});
-tom_highButton.addEventListener("click", () => {
-  audioTom_high.play();
-  tom_highButton.blur();
-});
-tom_lowButton.addEventListener("click", () => {
-  audioTom_low.play();
-  tom_lowButton.blur();
-});
-tom_midButton.addEventListener("click", () => {
-  audioTom_mid.play();
-  tom_midButton.blur();
-});
-
-//Events with keypress:
-document.body.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    audioSnare.play();
-  }
-  if (event.key === " ") {
-    audioKick.play();
-  }
-  if (event.key === "t") {
-    audioTom_high.play();
-  }
-  if (event.key === "r") {
-    audioTom_mid.play();
-  }
-  if (event.key === "e") {
-    audioTom_low.play();
-  }
-  if (event.key === "p") {
-    audioCrash.play();
-  }
-  if (event.key === "o") {
-    audioHihat_close.play();
-  }
-  if (event.key === "i") {
-    audioHihat_open.play();
-  }
-  if (event.key === "u") {
-    audioRide.play();
-  }
-});
-
-//Estilos
-let buttons = document.querySelectorAll(".buttons");
-console.log(buttons);
-for (let boton of buttons) {
-  boton.addEventListener("click", () => {
-    boton.style.color = "blue";
+//Styles and play:
+for (let btn of buttons) {
+  btn.addEventListener("click", () => {
+    //STYLES:
+    btn.style.color = "blue";
+    btn.style.transform = "scale(0.93)";
     setTimeout(() => {
-      boton.style.color = "inherit";
-    }, 300);
+      btn.style.color = "inherit";
+      btn.style.transform = "scale(1)";
+    }, 200);
+    //PLAY FUNCTION:
+    playSound(btn.id);
+    btn.blur();
   });
+}
+
+// Functions:
+function playSound(id) {
+  for (let audio of audios) {
+    const sound = audio;
+    if (id === sound.className) {
+      sound.play();
+    }
+  }
 }
